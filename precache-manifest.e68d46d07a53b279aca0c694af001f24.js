3d3dbfafb6cecc75192a2b04bd5ced38dd3d8e8d
abdullah <hbkamk@gmail.com>
2020-07-19 10:36:10 +0600

    Updates

---
+++ precache-manifest.e68d46d07a53b279aca0c694af001f24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25949f195134fa0008e4316e560688a",
+    "url": "/Flashcard-Quiz-App/index.html"
+  },
+  {
+    "revision": "f021654080ea7a78bd08",
+    "url": "/Flashcard-Quiz-App/static/css/main.a3c8938a.chunk.css"
+  },
+  {
+    "revision": "3bdab28c81b382faf351",
+    "url": "/Flashcard-Quiz-App/static/js/2.e7c60af2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Flashcard-Quiz-App/static/js/2.e7c60af2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f021654080ea7a78bd08",
+    "url": "/Flashcard-Quiz-App/static/js/main.045ec0ab.chunk.js"
+  },
+  {
+    "revision": "e9aa088428381c547a66",
+    "url": "/Flashcard-Quiz-App/static/js/runtime-main.54e0534c.js"
+  }
+]);
